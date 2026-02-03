12091e8c282f3b211d29e9ea5c4cb03dd4611758
Degen <degen@pumpsly.fun>
2026-02-03 18:47:43 +0300

    chore: update program ID to deployed devnet address

--- prediction-market/duels_escrow/programs/pumpsly/src/lib.rs
+++ prediction-market/duels_escrow/programs/pumpsly/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token::{self, Mint, Token, TokenAccount, Transfer};
 
-declare_id!("11111111111111111111111111111111");
+declare_id!("ErCo7q7uDZqeCE298u1XshQe4wxcDqdY88jfM9Z3B8rr");
 
 // Fee constants for duel resolution
 const DUEL_FEE_BPS: u64 = 250;  // 2.5% fee
